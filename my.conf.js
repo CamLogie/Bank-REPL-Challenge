2139a0170ad6745c13e3901556b13b5d2b594537
Cameron Logie <cameronlogie24@gmail.com>
2020-11-30 14:26:11 +0000

    setting up coding coverage

--- my.conf.js
+++ my.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      "src/*",
-      "tests/*""
+      './src/*',
+      './tests/spec/*'
     ],
 
 
@@ -28,13 +28,25 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      './src/*': ['coverage']
     },
+
+    plugins: [
+      'karma-jasmine',
+      'karma-coverage',
+      'karma-chrome-launcher'
+    ],
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
 
     // web server port
