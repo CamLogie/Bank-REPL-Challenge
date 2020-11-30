eafcd4dafbdd6039294d1f0091eae61b791086f8
Cameron Logie <cameronlogie24@gmail.com>
2020-11-30 15:36:43 +0000

    added method for type of transaction that classifies the transaction based on if the value is positive or negative

--- src/transaction.js
+++ src/transaction.js
@@ -1,5 +1,16 @@
 class Transaction {
   constructor(value) {
     this.value = value
+    this.timestamp = new Date().toLocaleDateString()
+    this.type = this.type_of_transaction()
+  }
+
+  type_of_transaction() {
+    if (this.value > 0) {
+      return 'credit'
+    }
+    else if (this.value < 0) {
+      return 'debit'
+    }
   }
 }
--- tests/spec/transaction_spec.js
+++ tests/spec/transaction_spec.js
@@ -3,4 +3,25 @@ describe('Transaction', () => {
     let transaction = new Transaction(400)
     expect(transaction.value).toBe(400)
   })
+
+  it('creates a timestamp upon construction', () => {
+    let transaction = new Transaction(400)
+    expect(transaction.timestamp).toBe(new Date().toLocaleDateString())
+  })
+
+  it('has method type_of_transaction that returns deposit if transaction value is positive', () => {
+    let transaction = new Transaction(400)
+    expect(transaction.type_of_transaction()).toBe('credit')
+  })
+
+  it('has method type_of_transaction that returns withdrawl if transaction value is negative', () => {
+    let transaction = new Transaction(-400)
+    expect(transaction.type_of_transaction()).toBe('debit')
+  })
+
+  it('classifies the type of transaction upon construction', () =>{
+    let transaction = new Transaction(400)
+    expect(transaction.type).toBe('credit')
+  })
+
 })
